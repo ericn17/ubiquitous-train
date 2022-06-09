bee1f490dd6c9ba25248f7c0c8e11cffe6492eb6
Eric Nguyen <15enguyen4@gmail.com>
2022-06-08 21:47:42 -0700

    added generateMarkdown functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+
+    This application is licensed by the following:
+
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseBadge(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## [Description](#table-of-contents)
+
+  ${data.why}
+
+  ${data.what}
+
+  ${data.how}
+
+  ## [Installation](#table-of-contents)
+
+  ${data.installation}
+
+  ## [Usage](#table-of-contents)
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ##[Contribution](#table-of-contents)
+
+  ${renderContributionSection(data.confirmContributers, data.contribute)}
+
+  ## [Tests](#table-of-contents)
+
+  ${data.test}
+
+  ## [Questions](#table-of-contents)
+
+  ${data.test}
+
+  ## [Questions](#table-of-contents)
+
+  Contact me through the following links:
+
+  [Github](https://github.com/${data.githubUsername})
+
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
